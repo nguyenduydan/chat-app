b1cee956ecb059cdfae56c40f2212b6a2e6702de
fogvn <dn135897@gmail.com>
2025-10-01 23:01:58 +0700

    fix path to file frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,10 +14,10 @@ const PORT = process.env.PORT || 3000;
 
 //make ready for deployment
 if (process.env.APP_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"));
     });
 }
 
