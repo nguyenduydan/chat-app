227c180aa64a5d5634336735f4d7e36a58f6e1c1
fogvn <dn135897@gmail.com>
2025-10-01 22:46:55 +0700

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -8,7 +9,17 @@ dotenv.config();
 
 //config
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
+
+//make ready for deployment
+if (process.env.APP_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/", "dist", "index.html"));
+    });
+}
 
 // middlewares
 
@@ -19,5 +30,5 @@ app.use('/api/messages', messageRoutes);
 // console.log(process.env.APP_ENV);
 // Start app
 app.listen(PORT, () =>
-    console.log(`Server running with port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 );
