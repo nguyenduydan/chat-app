36522ab7c46860212e792f8fdca1cbe3c86bba78
fogvn <dn135897@gmail.com>
2025-10-02 02:47:57 +0700

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,6 +8,10 @@ import { ENV } from "../lib/env.js";
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required" });
+    }
+
     try {
         const user = await User.findOne({ email });
         if (!user) return res.status(400).json({ message: "Invalid Credentials" });
