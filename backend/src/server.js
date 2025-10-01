d027a38df0568f4ca771b169314391709085a095
fogvn <dn135897@gmail.com>
2025-10-01 23:14:35 +0700

    fix api

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,22 +12,20 @@ const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
-//make ready for deployment
-if (process.env.APP_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"));
-    });
-}
-
-// middlewares
 
 //Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-// console.log(process.env.APP_ENV);
+//make ready for deployment
+if (process.env.APP_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/", "dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"));
+    });
+}
+
 // Start app
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
