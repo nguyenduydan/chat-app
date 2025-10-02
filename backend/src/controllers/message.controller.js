74f372cd9aa164b49fe160dc8496f14859ff58ca
fogvn <dn135897@gmail.com>
2025-10-03 01:43:02 +0700

    realtime messages implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getAllContacts = async (req, res) => {
     try {
@@ -79,6 +80,10 @@ export const sendMessage = async (req, res) => {
         await newMessage.save();
 
         // TODO: send message in real-time if user is online - socket.is
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(200).json(newMessage);
     } catch (error) {
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -20,6 +20,11 @@ io.use(socketAuthMiddleware);
 // this is for storig online users
 const userSocketMap = {}; // {userId:socketId}
 
+// we will use this function to check if the user is online or not
+export function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+}
+
 io.on("connection", (socket) => {
     console.log("A user connected:", socket.user.fullName);
 
